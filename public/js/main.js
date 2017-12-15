5b2592fe033ad585d4bae2bbd9020f0bb81f47a6
andyreesecups <andyreesecups@yahoo.com>
2017-12-14 18:31:47 -0600

    refactored code and updated timer

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ import Timer from './Timer.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
 import {createCollisionLayer} from './layers.js';
-import Keyboard from './KeyboardState.js';
+import {setupKeyboard} from './input.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -12,25 +12,13 @@ Promise.all([
     loadLevel('1-1'),
 ])
 .then(([mario, level]) => {
-    
-
-    const gravity = 2000;
     mario.pos.set(64, 64);
 
-    createCollisionLayer(level);
+    level.comp.layers.push(createCollisionLayer(level));
 
     level.entities.add(mario);
 
-
-    const SPACE = 32;
-    const input = new Keyboard();
-    input.addMapping(SPACE, keyState => {
-        if (keyState) {
-            mario.jump.start();
-        } else {
-            mario.jump.cancel();
-        }
-    });
+    const input = setupKeyboard(mario);
     input.listenTo(window);
 
     ['mousedown', 'mousemove'].forEach(eventName => {
@@ -42,13 +30,12 @@ Promise.all([
         });
     });
 
+
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
         level.update(deltaTime);
 
         level.comp.draw(context);
-
-        mario.vel.y += gravity * deltaTime;
     }
 
     timer.start();
